9e84d9e06d060702acc65f01ed0a656df5ef69f4
Lisa Thompson <lisa@Lisa-Thompsons-MacBook-Air.local>
2016-09-30 15:11:26 -0700

    moar things

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ $(document).on('ready', function(){
     $(this).css("background", "#626975");
   });
 });
+
+var questions_clicked = {};
+
+get_moar_cookies();
+
+function eat_cookies(key, value) {
+  questions_clicked[key] = value;
+  Cookies.set('questions_clicked', JSON.stringify(questions_clicked), { expires: 1});
+}
+
+function get_moar_cookies() {
+  values = Cookies.get('questions_clicked');
+  if (typeof values !== "undefined") {
+    user_settings = JSON.parse(values);
+  }
+}
